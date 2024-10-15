8eabff2ed16411195e0e172e75f2d9873ca30086
HyperPigeon <58276133+HyperPigeon@users.noreply.github.com>
2024-10-14 22:06:16 -0400

    Added chickensaur egg

--- common/src/main/java/net/hyper_pigeon/chickensaurs/register/BlockRegistry.java
+++ common/src/main/java/net/hyper_pigeon/chickensaurs/register/BlockRegistry.java
@@ -1,11 +1,15 @@
 package net.hyper_pigeon.chickensaurs.register;
 
+import net.hyper_pigeon.chickensaurs.block.ChickensaurEggBlock;
 import net.hyper_pigeon.chickensaurs.platform.Services;
 import net.minecraft.world.level.block.Block;
 
 import java.util.function.Supplier;
 
 public final class BlockRegistry {
+
+    public static final Supplier<ChickensaurEggBlock> CHICKENSAUR_EGG = registerBlock("chickensaur_egg", ChickensaurEggBlock::new);
+
     public static void init() {}
 
     private static <T extends Block> Supplier<T> registerBlock(String id, Supplier<T> block) {
--- common/src/main/java/net/hyper_pigeon/chickensaurs/register/ItemRegistry.java
+++ common/src/main/java/net/hyper_pigeon/chickensaurs/register/ItemRegistry.java
@@ -3,16 +3,13 @@ package net.hyper_pigeon.chickensaurs.register;
 import net.hyper_pigeon.chickensaurs.Constants;
 import net.hyper_pigeon.chickensaurs.platform.Services;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
+import net.minecraft.world.item.*;
 
 import java.util.function.Supplier;
 
 public final class ItemRegistry {
-
-    public static final Supplier<CreativeModeTab> MORETOTEMS_TAB = Services.PLATFORM.registerCreativeModeTab("chickensaurs_items", () -> Services.PLATFORM.newCreativeTabBuilder()
+    public static final Supplier<BlockItem> CHICKENSAUR_EGG = registerItem("chickensaur_egg", () -> new BlockItem(BlockRegistry.CHICKENSAUR_EGG.get(), new Item.Properties()));
+    public static final Supplier<CreativeModeTab> CHICKENSAURS_TAB = Services.PLATFORM.registerCreativeModeTab("chickensaurs_items", () -> Services.PLATFORM.newCreativeTabBuilder()
             .title(Component.translatable("itemGroup." + Constants.MOD_ID + ".chickensaurs_items"))
             .icon(() -> new ItemStack(Items.EGG))
             .displayItems((enabledFeatures, entries) -> {
