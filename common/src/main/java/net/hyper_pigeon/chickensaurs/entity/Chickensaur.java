ef274079036a02f39d60ab00d4975104275d1da4
whimsicalities <whimsicalities.code@gmail.com>
2024-10-27 15:12:58 +0000

    Baby entities except the hitbox is too small

--- common/src/main/java/net/hyper_pigeon/chickensaurs/entity/Chickensaur.java
+++ common/src/main/java/net/hyper_pigeon/chickensaurs/entity/Chickensaur.java
@@ -71,6 +71,8 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 
 public class Chickensaur extends TamableAnimal implements SmartBrainOwner<Chickensaur> {
+    private static final float BABY_SCALE = 0.5F;
+    private static final EntityDimensions BABY_DIMENSIONS = EntityRegistry.CHICKENSAUR.get().getDimensions().scale(BABY_SCALE);
     public float flap;
     public float flapSpeed;
     public float oFlapSpeed;
@@ -98,6 +100,16 @@ public class Chickensaur extends TamableAnimal implements SmartBrainOwner<Chicke
     }
 
     @Override
+    public EntityDimensions getDefaultDimensions(Pose pPose) {
+        return this.isBaby() ? BABY_DIMENSIONS : super.getDefaultDimensions(pPose);
+    }
+
+    @Override
+    public float getScale() {
+        return this.isBaby() ? BABY_SCALE : 1.0F;
+    }
+
+    @Override
     protected void defineSynchedData(SynchedEntityData.Builder pBuilder) {
         super.defineSynchedData(pBuilder);
         pBuilder.define(INTIMIDATING,false);
@@ -114,7 +126,7 @@ public class Chickensaur extends TamableAnimal implements SmartBrainOwner<Chicke
     @Nullable
     @Override
     public AgeableMob getBreedOffspring(ServerLevel serverLevel, AgeableMob ageableMob) {
-        return null;
+        return EntityRegistry.CHICKENSAUR.get().create(serverLevel);
     }
 
     public void aiStep() {
