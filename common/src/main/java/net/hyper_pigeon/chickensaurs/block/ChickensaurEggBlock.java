fe01822cec07d761b990beabd5cd1c3b91a9c162
HyperPigeon <58276133+HyperPigeon@users.noreply.github.com>
2024-10-26 21:51:01 -0400

    Chickensaurs will now attack players that break their eggs.

--- common/src/main/java/net/hyper_pigeon/chickensaurs/block/ChickensaurEggBlock.java
+++ common/src/main/java/net/hyper_pigeon/chickensaurs/block/ChickensaurEggBlock.java
@@ -1,8 +1,18 @@
 package net.hyper_pigeon.chickensaurs.block;
 
+import net.hyper_pigeon.chickensaurs.Chickensaurs;
+import net.hyper_pigeon.chickensaurs.Constants;
+import net.hyper_pigeon.chickensaurs.entity.Chickensaur;
+import net.minecraft.core.BlockPos;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.entity.monster.piglin.PiglinAi;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.material.PushReaction;
 
@@ -20,5 +30,12 @@ public class ChickensaurEggBlock extends Block {
                 .randomTicks()
                 .noOcclusion()
                 .pushReaction(PushReaction.DESTROY));
+    }
+
+    public BlockState playerWillDestroy(Level level, BlockPos pos, BlockState state, Player player) {
+        if (state.is(Constants.GUARDED_BY_CHICKENSAURS)) {
+            Chickensaur.angerNearbyChickensaurs(player,false);
+        }
+        return super.playerWillDestroy(level, pos, state, player);
     }
 }
